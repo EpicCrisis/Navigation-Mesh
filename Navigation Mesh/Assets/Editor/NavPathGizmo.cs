68e1e6aad62e85c95e1251049eae00b6810f7337
TerrenceChan <roxes2112@yahoo.com>
2017-10-12 18:00:26 +0800

    *nav mesh gizmo, obstacles, & jumping

---
+++ Navigation Mesh/Assets/Editor/NavPathGizmo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.AI;
+
+public class NavPathGizmo : MonoBehaviour
+{
+
+	[DrawGizmo (GizmoType.Selected | GizmoType.NonSelected)]
+	static void DrawNavPath (NavMeshAgent nav, GizmoType gizmoType)
+	{
+		
+		Gizmos.color = Color.red;
+
+		Vector3[] points = nav.path.corners;
+
+		for (int i = 0; i < points.Length - 1; i++) {
+
+			Gizmos.DrawLine (points [i], points [i + 1]);
+
+		}
+
+	}
+}
---
+++ Navigation Mesh/Assets/Scripts/CapsuleBoys.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CapsuleBoys : MonoBehaviour
+{
+
+	NavMeshAgent navMeshAgent;
+
+	public Transform target;
+
+	void Start ()
+	{
+		navMeshAgent = this.GetComponent<NavMeshAgent> ();
+	
+		StartCoroutine (RepathRoutine ());
+	}
+
+
+	void Update ()
+	{
+		
+	}
+
+	IEnumerator RepathRoutine ()
+	{
+		while (true) {
+			yield return new WaitForSeconds (0.5f);
+			navMeshAgent.SetDestination (target.position);
+		}
+	}
+}
